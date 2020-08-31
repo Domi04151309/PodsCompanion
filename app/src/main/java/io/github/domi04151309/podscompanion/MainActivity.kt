e3a235f562f87d4f1da2b891fcf52744364135fe
Domi04151309 <domi04151309business@gmail.com>
2020-08-31 10:58:05 +0200

    Add preference entries

--- app/src/main/java/io/github/domi04151309/podscompanion/MainActivity.kt
+++ app/src/main/java/io/github/domi04151309/podscompanion/MainActivity.kt
@@ -1,11 +1,39 @@
 package io.github.domi04151309.podscompanion
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings, PreferenceFragment())
+            .commit()
+    }
+
+    override fun onPreferenceStartFragment(caller: PreferenceFragmentCompat, pref: Preference): Boolean {
+        val fragment = supportFragmentManager.fragmentFactory.instantiate(
+            classLoader,
+            pref.fragment)
+        fragment.arguments = pref.extras
+        fragment.setTargetFragment(caller, 0)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.settings, fragment)
+            .addToBackStack(null)
+            .commit()
+        return true
+    }
+
+    class PreferenceFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            addPreferencesFromResource(R.xml.pref_general)
+
+        }
     }
 }

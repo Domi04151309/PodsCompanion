fa113c93960e1715ec82012f8de09ebcd5a60432
Domi04151309 <domi04151309business@gmail.com>
2020-09-02 09:45:59 +0200

    Add permissions and starter

---
+++ app/src/main/java/io/github/domi04151309/podscompanion/StartReceiver.kt
@@ -0,0 +1,14 @@
+package io.github.domi04151309.podscompanion
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class StartReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_MY_PACKAGE_REPLACED || intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            //TODO: Start service
+        }
+    }
+}

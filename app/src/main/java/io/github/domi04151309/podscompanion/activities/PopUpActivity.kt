de044ebac1ddbc8a641efdb6b96bd6a86d66863d
Domi04151309 <domi04151309business@gmail.com>
2020-09-03 18:04:42 +0200

    Use own layout for pop-up

--- app/src/main/java/io/github/domi04151309/podscompanion/activities/PopUpActivity.kt
+++ app/src/main/java/io/github/domi04151309/podscompanion/activities/PopUpActivity.kt
@@ -25,7 +25,7 @@ class PopUpActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_battery)
+        setContentView(R.layout.pop_up_status)
         localBroadcastManager = LocalBroadcastManager.getInstance(this)
     }
 

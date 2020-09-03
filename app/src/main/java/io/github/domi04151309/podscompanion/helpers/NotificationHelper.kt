6fa7a6163ab43a66fb73f68ecb6c25352cdf6999
Domi04151309 <domi04151309business@gmail.com>
2020-09-03 17:43:44 +0200

    Add rich notification

--- app/src/main/java/io/github/domi04151309/podscompanion/helpers/NotificationHelper.kt
+++ app/src/main/java/io/github/domi04151309/podscompanion/helpers/NotificationHelper.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Build
+import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
@@ -32,11 +33,12 @@ class NotificationHelper(private val context: Context) : SharedPreferences.OnSha
 
     fun showNotification() {
         if (shouldShowNotification) {
+            val loading = context.resources.getString(R.string.loading)
             notificationManager.notify(
                 NOTIFICATION_ID,
-                generateNotification(context.resources.getString(R.string.loading))
+                generateNotification(loading, loading, loading)
             )
-            LocalBroadcastManager.getInstance(context).sendBroadcast(Intent(PodsService.REQUEST_AIRPODS_BATTERY))
+            requestUpdate()
         }
     }
 
@@ -44,7 +46,7 @@ class NotificationHelper(private val context: Context) : SharedPreferences.OnSha
         if (shouldShowNotification) {
             notificationManager.notify(
                 NOTIFICATION_ID,
-                generateNotification(context.resources.getString(R.string.status_text, left, case, right))
+                generateNotification(left, case, right)
             )
         }
     }
@@ -67,17 +69,37 @@ class NotificationHelper(private val context: Context) : SharedPreferences.OnSha
                 shouldShowNotification = false
                 cancelNotification()
             }
+        } else if (key == PREF_NOTIFICATION_STYLE) {
+            requestUpdate()
         }
     }
 
-    private fun generateNotification(text: String): Notification {
-        return NotificationCompat.Builder(context, CHANNEL_ID)
-            .setContentText(text)
+    private fun requestUpdate() {
+        LocalBroadcastManager.getInstance(context).sendBroadcast(Intent(PodsService.REQUEST_AIRPODS_BATTERY))
+    }
+
+    private fun generateNotification(left: String, case: String, right: String): Notification {
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_pods_white)
             .setShowWhen(false)
             .setOngoing(true)
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-            .build()
+        when (prefs.getString(PREF_NOTIFICATION_STYLE, PREF_NOTIFICATION_STYLE_RICH)) {
+            PREF_NOTIFICATION_STYLE_RICH -> {
+                val disconnectedLong = context.resources.getString(R.string.unknown_status)
+                val disconnectedShort = context.resources.getString(R.string.unknown_status_short)
+                val views = RemoteViews(context.packageName, R.layout.notification_status)
+                views.setTextViewText(R.id.txt_left, if (left == disconnectedLong) disconnectedShort else left)
+                views.setTextViewText(R.id.txt_case, if (case == disconnectedLong) disconnectedShort else case)
+                views.setTextViewText(R.id.txt_right, if (right == disconnectedLong) disconnectedShort else right)
+                builder.setStyle(NotificationCompat.DecoratedCustomViewStyle())
+                builder.setContent(views)
+            }
+            PREF_NOTIFICATION_STYLE_TEXT_ONLY -> {
+                builder.setContentText(context.resources.getString(R.string.status_text, left, case, right))
+            }
+        }
+        return builder.build()
     }
 
     private fun createNotificationChannel() {
@@ -97,5 +119,8 @@ class NotificationHelper(private val context: Context) : SharedPreferences.OnSha
         private const val NOTIFICATION_ID = 50
         private const val PREF_SHOW_NOTIFICATION = "show_notification"
         private const val PREF_SHOW_NOTIFICATION_DEFAULT = true
+        private const val PREF_NOTIFICATION_STYLE = "notification_style"
+        private const val PREF_NOTIFICATION_STYLE_RICH = "rich"
+        private const val PREF_NOTIFICATION_STYLE_TEXT_ONLY = "text"
     }
 }
